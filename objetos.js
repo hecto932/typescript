c3de0f8c245db3145ff1c35aaa37666a24a05199
Hector Flores <hecto932@gmail.com>
2017-03-11 23:39:47 -0600

    Interpolacion

--- objetos.js
+++ objetos.js
@@ -1,11 +1,14 @@
 var MyPerson = (function () {
     function MyPerson(_first_name, _last_name) {
         console.log(_first_name, _last_name);
-        /*this.first_name = _first_name
-        this.last_name = _first_name*/
+        this.first_name = _first_name;
+        this.last_name = _first_name;
     }
+    MyPerson.prototype.getSaludo = function () {
+        var emojis = ':)';
+        return "Saludos:\n        " + this.first_name + " " + this.last_name + ",\n        te enviamos un emoji de la consola.\n        " + emojis;
+    };
     return MyPerson;
 }());
-var personaUno = new MyPerson();
-var personaDos = new MyPerson('Hector');
 var personaTres = new MyPerson('Hector', 'Flores');
+console.log(personaTres.getSaludo());
--- objetos.ts
+++ objetos.ts
@@ -3,11 +3,18 @@ class MyPerson{
     last_name:string
     constructor(_first_name?:string, _last_name?:string){
         console.log(_first_name, _last_name)
-        /*this.first_name = _first_name
-        this.last_name = _first_name*/
+        this.first_name = _first_name
+        this.last_name = _first_name
+    }
+
+    getSaludo():string{
+        let emojis = ':)'
+        return `Saludos:
+        ${this.first_name} ${this.last_name},
+        te enviamos un emoji de la consola.
+        ` + emojis
     }
 }
 
-let personaUno = new MyPerson()
-let personaDos = new MyPerson('Hector')
 let personaTres = new MyPerson('Hector', 'Flores')
+console.log(personaTres.getSaludo())
